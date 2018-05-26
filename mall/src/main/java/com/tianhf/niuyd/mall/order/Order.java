961f3b24c2ca48ccded8e5414654b97a7ffdca47
‘田海峰 <tianhf@guahao.com>
2018-05-26 16:47:55 +0800

    ceshi

---
+++ mall/src/main/java/com/tianhf/niuyd/mall/order/Order.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2001-2018 GuaHao.com Corporation Limited. All rights reserved. 
+ * This software is the confidential and proprietary information of GuaHao Company. 
+ * ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only 
+ * in accordance with the terms of the license agreement you entered into with GuaHao.com.
+ */
+package com.tianhf.niuyd.mall.order;
+
+/**
+ * @author tianhf
+ * @version V1.0
+ * @type Order
+ * @Desc 订单
+ * @email tianhf@guahao.com
+ * @since 2018-05-26 16:43
+ */
+public class Order {
+
+
+}
